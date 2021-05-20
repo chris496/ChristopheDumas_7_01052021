24e3e2f8cf0bd7da149cb1885b22a0aa7e0c4356
Unknown <christophe.dumas1@gmail.com>
2021-05-20 21:22:49 +0200

    configuration vuejs

--- frontend/groupomania/src/main.js
+++ frontend/groupomania/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAddressCard, faUsers, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).mount('#app')
+library.add(faAddressCard, faUsers, faSignOutAlt);
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount('#app')
